4993f0260dd7e823a5f8b6b72dcc5afc9e9b036d
vliupro <virus_liu@163.com>
2016-03-11 22:03:06 +0800

    fix sql and improve the speed

--- lagou/lagou.py
+++ lagou/lagou.py
@@ -6,20 +6,134 @@ from queue import Queue
 import requests
 from bs4 import BeautifulSoup
 
-from lagouDb import DB
+from lagouDb import DB, DbTools
 
 
 kd_queue = Queue()
+ps_queue = Queue()
+lock = threading.Lock()
 db = DB(host='127.0.0.1', user='vliupro', passwd='liujida', database='ponytest')
 
+
+class Lagou:
+    def __init__(self):
+        self.URL = 'http://www.lagou.com/'
+
+    def getTypes(self):
+        positions = []
+        page = self.getPagecode(self.URL)
+        if page == None:
+            print('No content ...')
+            return []
+        bs = BeautifulSoup(page.content, 'lxml')
+        for s in bs.find_all('div', 'menu_sub'):
+            for i in s.select('dl dd a'):
+                positions.append(i.string)
+        if len(positions):
+            print('职业列表获取完成')
+            return positions
+        else:
+            print('职业列表获取失败')
+
+    def getCities(self):
+        cities = []
+        page = self.getPagecode('http://www.lagou.com/jobs/list_Java')
+        if page == None:
+            print('No content city')
+            return []
+        bs = BeautifulSoup(page.content, 'lxml')
+        for s in bs.find_all('div', 'more-positions'):
+            for i in s.select('li a'):
+                cities.append(i.string)
+        cities.pop(0)
+        return cities
+
+    def getPagecode(self, url, time=0):
+        try:
+            return requests.get(url)
+        except Exception:
+            print('connect failed ...')
+            return None
+
+
+
 class ThreadCrawl(threading.Thread):
-    def __init__(self, queue):
+    def __init__(self, kqueue, pqueue):
         threading.Thread.__init__(self)
-        self.queue = queue
+        self.kdqueue = kqueue
+        self.psqueue = pqueue
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+        }
+        self.url = 'http://www.lagou.com/jobs/positionAjax.json?px=new'
+
+    def getJsonData(self, kd, pn):
+        data = {
+            'kd': kd,
+            'pn': pn,
+            'px': 'new'
+        }
+        print('get jsonData , kd = ' + str(kd) + ' , pn = ' + str(pn))
+        return requests.post(self.url, data=data, headers=self.headers).json()
 
     def run(self):
         while True:
-            item = self.queue.get()
+            item = self.kdqueue.get()
+            totalPageCount = int(self.getJsonData(item, 1)['content']['totalPageCount'])
+            print('get ' + str(item) + ' data , total = ' + str(totalPageCount))
+            for i in range(1, totalPageCount + 1):
+                jd = self.getJsonData(item, i)
+                self.psqueue.put(jd['content']['result'])
+            self.kdqueue.task_done()
+
+
+class ThreadSave(threading.Thread):
+    def __init__(self, queue, lock, dt):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.lock = lock
+        self.dt = dt
+
+    def run(self):
+        while True:
+            jobs = self.queue.get()
+            print('save data , kd = ' + str(jobs[0]['positionType']))
+            with lock:
+                dt.save(jobs)
             self.queue.task_done()
 
+
+if __name__ == '__main__':
+    lg = Lagou()
+    print('get positions list ...')
+    plist = lg.getTypes()
+    # for p in plist:
+        # print(p)
+    cities = lg.getCities()
+    dt = DbTools(db)
+    if len(plist) != 0:
+        for p in plist:
+            dt.positiontype_save(p)
+            kd_queue.put(p)
+    if len(cities) != 0:
+        dt.city_save(cities)
+
+    num = 4
+    for i in range(num):
+        t = ThreadCrawl(kd_queue, ps_queue)
+        t.setDaemon(True)
+        t.start()
+        s = ThreadSave(ps_queue, lock, dt)
+        s.setDaemon(True)
+        s.start()
+
+    # for i in range(num):
+        # t = ThreadSave(ps_queue, lock, dt)
+        # t.setDaemon(True)
+        # t.start
+
+    kd_queue.join()
+    ps_queue.join()
+
+    print('crawl ending ...')
 
--- lagou/lagouDb.py
+++ lagou/lagouDb.py
@@ -8,36 +8,34 @@ db = Database()
 class PositionType(db.Entity):
     _table_ = 'db_type'
     typeName = Required(str, unique=True)
-    # 区别职位类型和总类型 比如： Java， 技术
-    typeNo = Required(int)
 
 
 class City(db.Entity):
     _table_ = 'db_city'
     name = Required(str, unique=True)
-    job = Set('Job')
+    # job = Set('Job')
 
 
 class Job(db.Entity):
     _table_ = 'db_job'
-    positionId = Required(int, size=32, unique=True)
+    positionId = Required(str, unique=True)
     positionName = Required(str)
     positionFirstType = Optional(str)
     positionType = Optional(str)
     positionAdvantage = Optional(str)
-    salary = Optional(str)
+    salary = Required(str)
     workYear = Optional(str)
     education = Optional(str)
     createTime = Optional(str)
     nature = Optional(str)
     leader = Optional(str)
-    city = Required(City)
-    company = Required('Company')
+    city = Optional(str)
+    company = Optional('Company')
 
 
 class Company(db.Entity):
     _table_ = 'db_company'
-    companyId = Required(int, size=32, unique=True)
+    companyId = Required(str, unique=True)
     companyShortName = Required(str)
     companyName = Required(str)
     companySize = Optional(str)
@@ -45,7 +43,7 @@ class Company(db.Entity):
     financeStage = Optional(str)
     industryField = Optional(str)
     companyAdvlist = Optional(str)
-    jobs = Set(Job)
+    jobs = Set('Job')
 
 
 class DB:
@@ -59,12 +57,20 @@ class DB:
         self.db.generate_mapping(create_tables=True)
 
     @db_session
+    def check_city(self, city):
+        return City.exists(name=city)
+
+    @db_session
+    def check_type(self, p):
+        return City.exists(name=p)
+
+    @db_session
     def check_job(self, positionId):
-        return Job.exists(positionId=int(positionId))
+        return Job.exists(positionId=positionId)
 
     @db_session
     def check_company(self, companyId):
-        return Company.exists(companyId=int(companyId))
+        return Company.exists(companyId=companyId)
 
 
 class DbTools():
@@ -74,11 +80,13 @@ class DbTools():
     @db_session
     def city_save(self, cities):
         for city in cities:
-            c = City(name=city)
+            if City.get(name=city) == None:
+                c = City(name=city)
 
     @db_session
-    def positiontype_save(self, typename, typeno):
-        ptype = PositionType(typeName=typename, typeNo=typeno)
+    def positiontype_save(self, typename):
+        if PositionType.get(typeName=typename) == None:
+            ptype = PositionType(typeName=typename)
 
     @db_session
     def company_save(self, cp):
@@ -91,29 +99,29 @@ class DbTools():
     @db_session
     def save(self, positions):
         for position in positions:
-            if not self.db.check_company(companyId=position['companyId']):
+            if not self.db.check_company(companyId=str(position['companyId'])):
                 s = ','
                 if len(position['companyLabelList']) == 0:
                     ads = ''
                 else:
                     ads = ','.join([i for i in position['companyLabelList']])
                 com = {
-                    'companyId': int(position['companyId']),
+                    'companyId': str(position['companyId']),
                     'companyShortName': position['companyName'],
                     'companyName': position['companyShortName'],
                     'companySize': position['companySize'],
                     'companyLogo': position['companyLogo'],
                     'financeStage': position['financeStage'],
                     'industryField': position['industryField'],
-                    'companyAdvList': ads
+                    'companyAdvlist': ads
                 }
                 company = self.company_save(com)
             else:
                 pass
 
-            if not self.db.check_job(positionId=position['positionId']):
+            if not self.db.check_job(positionId=str(position['positionId'])):
                 pt = {
-                    'positionId': position['positionId'],
+                    'positionId': str(position['positionId']),
                     'positionName': position['positionName'],
                     'positionFirstType': position['positionFirstType'],
                     'positionType': position['positionType'],
@@ -123,11 +131,21 @@ class DbTools():
                     'education': position['education'],
                     'createTime': position['createTime'],
                     'nature': position['jobNature'],
-                    'leader': position['leaderName']
+                    'leader': position['leaderName'],
+                    'city': position['city']
                 }
                 job = self.position_save(pt)
-                job.city = City.get(name=position['city'])
-                job.company = Company.get(companyId=position['companyId'])
-                Company.get(companyId=position['companyId']).jobs = job
+                # city = City.get(name=position['city'])
+                # if city != None:
+                #     job.city = city
+                # else:
+                #     job.city = position['city']
+
+                company = Company.get(companyId=str(position['companyId']))
+                if company != None:
+                    job.company = company
+                    company.jobs = job
+                else:
+                    job.company = position['companyId']
             else:
                 pass
